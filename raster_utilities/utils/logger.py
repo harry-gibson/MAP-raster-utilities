7ba0e1945b15be778c8dc8ac47cc9bc8a43a5594
harry-gibson <harry.s.gibson@gmail.com>
2019-07-23 14:53:45 +0100

    Update messagelogger to be class

--- raster_utilities/utils/logger.py
+++ raster_utilities/utils/logger.py
@@ -3,11 +3,19 @@ from enum import Enum
 class LogLevels(Enum):
     DEBUG = 10
     WARNING = 20
+    INFO = 30
     ERROR = 40
     NONE = 100
 
-def logMessage(message, level="info", newline=True):
-    if newline:
-        print(message)
-    else:
-        print(message, end="")
+
+class MessageLogger:
+    def __init__(self, logLevel=LogLevels.INFO):
+        self._logLevel = logLevel
+
+    def logMessage(self, message, level=LogLevels.INFO, newline=True):
+        if level.value < self._logLevel.value:
+            return
+        if newline:
+            print(str(level.name).ljust(8) + ": | " + message)
+        else:
+            print(str(level.name).ljust(8) + ": | " + message, end="")
