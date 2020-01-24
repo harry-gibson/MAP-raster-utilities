321978d3d4823be66f839d884b4f1e20d2062147
harry-gibson <harry.s.gibson@gmail.com>
2020-01-24 14:05:01 +0000

    tidied up TemporalAggregator

--- raster_utilities/aggregation/temporal/TemporalAggregator.py
+++ raster_utilities/aggregation/temporal/TemporalAggregator.py
@@ -13,18 +13,34 @@ class TemporalAggregator:
     def __init__(self, filesDict, outFolder, outputNDV, stats, doSynoptic, bytesLimit=40e9, loggingLevel = LogLevels.INFO):
         '''Runs temporal aggregation across a supplied dictionary of filenames
 
-        The aggregation doesn't strictly need to be temporal, this is strictly just about summarising
+        The aggregation doesn't strictly need to be temporal, rather this is about summarising
         a number of rasters with identical shape into a single file of the same shape.
 
         The input dictionary should have keys that define the output filename, with values that are lists of filenames
         contributing to that output file. Specifically the keys should be the output filename with a '*' character that will
-        be replaced by the name of the aggregation statistic in each file.
+        be replaced by the name of the aggregation statistic in each file, e.g.:
+            filesDict = {'MyVariableName.2000.Annual.*.1km.Data': [list of all files in year 2000]}
+        Client code can therefore generate the list of files by calculating what the key will be then using it as a
+        glob pattern to get a list of all matching files.
 
         All of the input files (all the members of the all the values for each key) should have the same
         dimensions and geotransform. This requirement is so that synoptic outputs can be calculated on the fly if required
         e.g. the mean etc of all files present in any of the keys.
 
+        outputNDV doesn't have to match the incoming NDV
+
+        stats is a list containing some or all of the values of the TemporalAggregationStats enumeration, or strings
+        ["min", "max", "mean", "sd", "sum", "count"]
+        The more statistics are specified, the more memory is required.
+
+        doSynoptic specifies whether "overall" statistics should be calculated in addition to one
+        per timestep - this doubles memory use, but means you don't have to calculate synoptic results separately by
+        passing in a synoptic key with all files listed, thus speeding things up.
+        This has no effect if filesDict only has one item. The synoptic calculation will only count each input file
+        once, regardless of how many times it appears (e.g. it may be used in both annual and monthly summaries but
+        will only count once towards the synoptic output)
         '''
+
         assert isinstance(filesDict, dict)
         self.filesDict = filesDict
         assert isinstance(outFolder, str)
@@ -122,33 +138,12 @@ class TemporalAggregator:
     def RunAggregation(self, deleteIntermediateTiles=False):
         '''For each key in filesDict, aggregates the files on the associated value to the specified stats.
 
-        Returns true if the aggregation needed to be done in multiple tiles for memory reasons in
-        which case you will need to mosaic the out  put tiles afterwards.
+        The input files and the outputs they contribute to have been set in the constructor, see comments there.
 
-        Files should be provided in filesDict; each item should be a key representing an
-        output point (timespan) and a value which is a list representing the file paths for that point in time.
-        The keys could be months (e.g. "2001-02"), years (e.g. "2002"), synoptic months
-        (e.g. "February"), or anything else. If there is only one item then only synoptic outputs
-        can be generated. The string version of the key will be used to generate the output filenames
-        in the form "StringKey.Stat.Resolution(.TileId).tif"
+        If the aggregation is too large to be done in memory (according to a very inaccurate estimation), it will
+        be done in multiple tiles and then an attempt will be made to mosaic these tiles afterwards.
 
         All of the files (across all output time points) must have the same geotransform and extent.
-
-        top and bottom allow specification of a subset (horizontal slice) of the files to run the
-        aggregation for, in case there isn't enough memory to do the whole extent in one go. In this
-        case output files will have a suffix indicating the top pixel coordinate relative to the incoming
-        files.
-
-        width must match the overall width of the incoming files (vertical slicing isn't supported)
-
-        outputNDV doesn't have to match the incoming NDV
-
-        stats is a list containing some or all of the values from aggregation_values.TemporalAggregationStats
-
-        The more statistics are specified, the more memory is required.
-
-        doSynoptic specifies whether "overall" statistics should be calculated in addition to one
-        per timestep - this doubles memory use. This has no effect if filesDict only has one item.
         '''
 
         self._outputFilesTiles = defaultdict(list)
@@ -180,6 +175,11 @@ class TemporalAggregator:
             self._logger.logMessage("You can delete the tile files from the aggregation_tiles subfolder", LogLevels.INFO)
 
     def _stitchTiles(self, deleteTiles=False):
+        ''' Attempts to mosaic intermediate tiles of aggregated output into complete files.
+        Relies on gdal binaries (gdalbuildvrt, gdal_translate, gdaladdo, and gdalinfo) being present in the path
+        :param deleteTiles: bool, should the intermediate files be deleted after mosaicing?
+        :return:
+        '''
         vrtBuilder = "gdalbuildvrt {0} {1}"
         transBuilder = "gdal_translate -of GTiff -co COMPRESS=LZW " + \
                        "-co PREDICTOR=2 -co TILED=YES -co BIGTIFF=YES " + \
@@ -189,21 +189,7 @@ class TemporalAggregator:
         statBuilder = "gdalinfo -stats {0}"
         vrts = []
         tifs = []
-        #for stat in self.stats:
-        #    statname = stat.value
-        #    timeKeys = self._timePoints()
-        #    ranSynoptic = (len(self.filesDict.keys()) > 1) and self.doSynoptic
-        #    if ranSynoptic and ("Overall" not in timeKeys):
-        #        timeKeys.append("Overall")
-        #    for timeKey in timeKeys:
-        #        tiffWildCard = self._fnGetter(str(timeKey), stat,  "*")
-        #        sliceTiffs = os.path.join(self._tileFolder, tiffWildCard)
-        #        vrtName = timeKey + "." + statname + ".vrt"
-        #        vrtFile = os.path.join(self.outFolder, vrtName)
-        #        vrtCommand = vrtBuilder.format(vrtFile, sliceTiffs)
-        #        self._logger.logMessage("Building vrt " + vrtFile, LogLevels.INFO)
-        #        vrts.append(vrtFile)
-        #        subprocess.call(vrtCommand)
+
         for outname, tiles in self._outputFilesTiles.items():
             vrtFile = outname.replace(".tif", ".vrt")
             vrtPath = os.path.join(self.outFolder, vrtFile)
@@ -239,40 +225,21 @@ class TemporalAggregator:
             subprocess.check_output(statCommand)
 
     def _temporalAggregationSliceRunner(self,  top, bottom, outFolder):
-        '''Runs temporal aggregation across a set of files.
+        '''Runs temporal aggregation across a set of files, for a specified horizontal slice
 
-        Files should be provided in filesDict; each item should be a key representing an
-        output point (timespan) and a value representing the file paths for that point in time.
-        The keys could be months (e.g. "2001-02"), years (e.g. "2002"), synoptic months
-        (e.g. "February"), or anything else. If there is only one item then only synoptic outputs
-        can be generated.
-
-        All of the files (across all output time points) must have the same geotransform and extent.
+        Not intended to be called by client code - use RunAggregation()
 
         top and bottom allow specification of a subset (horizontal slice) of the files to run the
         aggregation for, in case there isn't enough memory to do the whole extent in one go. In this
         case output files will have a suffix indicating the top pixel coordinate relative to the incoming
         files.
-
-        width must match the overall width of the incoming files (vertical slicing isn't supported)
-
-        outputNDV doesn't have to match the incoming NDV
-
-        stats is a list containing some or all of
-        ["min", "max", "mean", "sd", "sum", "count"]
-        The more statistics are specified, the more memory is required.
-
-        doSynoptic specifies whether "overall" statistics should be calculated in addition to one
-        per timestep - this doubles memory use. This has no effect if filesDict only has one item.
         '''
 
-        #if not (isinstance(bottom, int) and isinstance(top, int)):
-         #   raise TypeError("top and bottom must be integer values")
         logMessage = self._logger.logMessage("Beginning slice at top: {} , bottom: {}".format(top, bottom),
                                              LogLevels.DEBUG)
         if not ((bottom > top ) and top >= 0):
-            raise ValueError("bottom must be greater than top and top must be GTE zero " +
-                             "(raster origin is the top-left corner)")
+            raise ValueError("bottom must be greater than top, and top must be >= zero " +
+                             "(because raster origin of 0,0 is the top-left corner)")
         sliceHeight = bottom - top
 
         runSynoptic = (len(self.filesDict.keys()) > 1) and self.doSynoptic
@@ -293,58 +260,36 @@ class TemporalAggregator:
             where = top
 
         for timeKey, timeFiles in self.filesDict.items():
-            #for stat in self.stats:
-            #    filenameWillBe = self._fnGetter(str(timeKey), stat, where)
 
             self._logger.logMessage(timeKey, LogLevels.DEBUG)
 
             allDone = True
 
-            # see if we can use thet mastergrid filename generatore for these inputs, which relies on getting the
-            # output filename from the inputs, so they must all be the same; all this really does is check that all
-            # the inputs are the same except for in the 2nd and 3rd tokens
-            # testFilename = None
-            # useMGFilenameGenerator = True
-            # for inputFile in timeFiles:
-            #     try:
-            #         thisOutputFilename = self._fnGetter_MG(inputFile, timeKey, self.stats[0], -1)
-            #         if testFilename is None:
-            #             testFilename = thisOutputFilename
-            #         elif testFilename != thisOutputFilename:
-            #             useMGFilenameGenerator = False
-            #             break
-            #     except ValueError:
-            #         useMGFilenameGenerator = False
-            #         break
-
             for stat in self.stats:
-                # if useMGFilenameGenerator:
-                #     fnWillBe = os.path.join(outFolder, self._fnGetter_MG(timeFiles[0], timeKey, stat, where))
-                # else:
-                #     fnWillBe = os.path.join(outFolder, self._fnGetter_Safe(str(timeKey), stat, where))
                 fnWillBe = self._fnGetter(timeKey, stat)
                 if not os.path.exists(fnWillBe):
                     allDone = False
             if allDone:
-                self._logger.logMessage("All outputs for {0!s} already exist, skipping!".format(timeKey), LogLevels.WARNING)
+                self._logger.logMessage("All outputs for {0!s} already exist, skipping!".format(timeKey),
+                                        LogLevels.WARNING)
                 continue
 
             for timeFile in timeFiles:
                 self._logger.logMessage("Reading {}".format(timeFile), LogLevels.DEBUG)
                 data, thisGT, thisProj, thisNdv = ReadAOI_PixelLims(timeFile, None, (top, bottom))
                 if data.dtype != 'float32':
-                    self._logger.logMessage("Data will be cast to float32, precision or data may be lost", LogLevels.WARNING)
+                    self._logger.logMessage("Data will be cast to float32, precision or data may be lost",
+                                            LogLevels.WARNING)
                     if thisNdv < np.finfo(np.float32).min or thisNdv > np.finfo(np.float32).max:
                         self._logger.logMessage("Nodata value outside of range of float32, will be reset to -9999",
                                                 LogLevels.WARNING)
-                        data[data==thisNdv]=-9999
+                        data[data==thisNdv] = -9999
                         thisNdv = -9999
                     data = data.astype('float32')
                 if sliceGT is None:
                     # first file
                     sliceGT = thisGT
                     sliceProj = thisProj
-
                 else:
                     if sliceGT != thisGT or sliceProj != thisProj:
                         raise ValueError("File " + timeFile +
@@ -352,22 +297,16 @@ class TemporalAggregator:
                 # pass the filename as a tag so the aggregator can avoid using the same file
                 # multiple times in calculating overall values. E.g. we might be calculating annual
                 # and, separately, monthly means, involving reading each file twice, but only want
-                # to count each one once towards overall totals.
+                # to count each one once towards overall synoptic totals if they are also being tracked.
                 statsCalculator.addFile(data, thisNdv, timeFile)
             periodResults = statsCalculator.emitStep()
             for stat in self.stats:
-                #if useMGFilenameGenerator:
-                #    fullFnWillBe = os.path.join(outFolder, self._fnGetter_MG(timeFiles[0], timeKey, stat, -1))
-                #    fnWillBe = os.path.join(outFolder, self._fnGetter_MG(timeFiles[0], timeKey, stat, where))
-                #else:
-                #    fullFnWillBe = os.path.join(outFolder, self._fnGetter_Safe(str(timeKey), stat, -1))
-                #    fnWillBe = os.path.join(outFolder, self._fnGetter_Safe(str(timeKey), stat, where))
                 tileFNWillBe = self._fnGetter(timeKey, stat, where)
                 fullFnWillBe = self._fnGetter(timeKey, stat, -1)
                 if not os.path.exists(tileFNWillBe):
                     SaveLZWTiff(periodResults[stat], self.outputNDV, sliceGT, sliceProj, outFolder,
                             tileFNWillBe)
-                    self._outputFilesTiles[fullFnWillBe].append(os.path.join(outFolder,tileFNWillBe))
+                    self._outputFilesTiles[fullFnWillBe].append(os.path.join(outFolder, tileFNWillBe))
         if runSynoptic:
             overallResults = statsCalculator.emitTotal()
             if isFullFile:
@@ -376,12 +315,6 @@ class TemporalAggregator:
                 where = top
             timeKey = "Synoptic.Overall"
             for stat in self.stats:
-                #try:
-                #    fullFnWillBe = os.path.join(outFolder, self._fnGetter_MG(timeFiles[0], timeKey, stat, -1))
-                #    fnWillBe = os.path.join(outFolder, self._fnGetter_MG(timeFiles[0], timeKey, stat, where))
-                #except:
-                #    fullFnWillBe = os.path.join(outFolder, self._fnGetter_Safe(str(timeKey), stat, -1))
-                #    fnWillBe = os.path.join(outFolder, self._fnGetter_Safe(str(timeKey), stat, where))
                 tileFNWillBe = self._fnGetter(timeKey, stat, where)
                 fullFnWillBe = self._fnGetter(timeKey, stat, -1)
 
